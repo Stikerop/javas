f89286ef3967421301387e3032a353fb1abea3a4
Artem Soldatenko <soldatenko.artem@gmail.com>
2022-09-22 14:38:46 +0300

    First lesson 4

--- script.js
+++ script.js
@@ -1,36 +1,53 @@
-// 'use strict';
+// const hamb = 5;
+// const fries = 0;
 //
-// if (1) {
-//     console.log('Ok!');
-// } else {
-//     console.log('Error');
-// };
-
-// const num = 50;
-
-// if (num < 49){
-//     console.log('Error');
-// } else  if (num > 100) {
-//     console.log('Много');
-// }  else {
-//     console.log('Ok!');
+// if (hamb && fries) {
+//     console.log('Я сыт');
 // }
+
+// const hamb = 3;
+// const fries = 1;
+// const cola = 1;
 //
-// (num === 50) ? console.log('Ok!') : console.log('Error');
+// console.log(hamb === 3 && cola && fries)
+//
+// console.log( 1 && 0);
+// console.log( 1 && 5);
+// console.log( null && 5);
+// console.log( 0 && 'asdasdsadsadas');
+//
+// if (hamb === 3 && cola === 1 && fries ) {
+//     console.log('Все сыты');
+// } else {
+//     console.log('Уходим');
+// }
 
-const num = '50';
+//
+// const hamb = 3;
+// const fries = 3;
+// const cola = 0;
+// const nog = 2;
+//
+// if (hamb === 3 && cola === 2 || fries === 3 && nog ) {
+//     console.log('Все довольны');
+// } else {
+//     console.log('Уходим');
+// }
+// console.log( hamb === 3 && cola === 1 || fries === 3 && nog);
+//
+// let johnRep,  alexRep,  mariaRep  = 'done';
+//
+// console.log(johnRep || alexRep || mariaRep);
+//
+// console.log(!0);
 
-switch (num) {
-    case '49':
-        console.log('Неверно');
-        break;
-    case '100':
-        console.log('Неверно');
-        break;
-    case '50':
-        console.log('Правдивое');
-        break;
-    default:
-        console.log('Не в этот раз');
-        break;
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger && cola || fries === 3 && nuggets) {
+    console.log('Done!')
 }
