ecebfd8a834eaa10c1e1e13607187a89bf4c8942
Artem Soldatenko <soldatenko.artem@gmail.com>
2022-09-22 14:56:06 +0300

    First lesson 5

--- script.js
+++ script.js
@@ -1,53 +1,22 @@
-// const hamb = 5;
-// const fries = 0;
+'use strict'
+
+let num = 50;
 //
-// if (hamb && fries) {
-//     console.log('Я сыт');
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
 // }
 
-// const hamb = 3;
-// const fries = 1;
-// const cola = 1;
-//
-// console.log(hamb === 3 && cola && fries)
-//
-// console.log( 1 && 0);
-// console.log( 1 && 5);
-// console.log( null && 5);
-// console.log( 0 && 'asdasdsadsadas');
-//
-// if (hamb === 3 && cola === 1 && fries ) {
-//     console.log('Все сыты');
-// } else {
-//     console.log('Уходим');
+// do {
+//     console.log(num);
+//     num++;
 // }
+// while (num < 55);
 
-//
-// const hamb = 3;
-// const fries = 3;
-// const cola = 0;
-// const nog = 2;
-//
-// if (hamb === 3 && cola === 2 || fries === 3 && nog ) {
-//     console.log('Все довольны');
-// } else {
-//     console.log('Уходим');
-// }
-// console.log( hamb === 3 && cola === 1 || fries === 3 && nog);
-//
-// let johnRep,  alexRep,  mariaRep  = 'done';
-//
-// console.log(johnRep || alexRep || mariaRep);
-//
-// console.log(!0);
-
-
-
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-
-if (hamburger && cola || fries === 3 && nuggets) {
-    console.log('Done!')
+for (let i = 1; i < 10; i++) {
+    if (i === 6 ) {
+        // break;
+        continue;
+    }
+    console.log(i);
 }
