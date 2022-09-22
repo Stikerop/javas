e3fe468d0095c8341fcaf0a5fcdb891a80a099b1
Artem Soldatenko <soldatenko.artem@gmail.com>
2022-09-22 11:53:55 +0300

    First lesson

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-GHbdtn
+const numberOfFilms = +prompt("Сколько фильмов вы уже посомтрели", " ");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const   a = prompt("Сколько фильмов вы уже посомтрели", ""),
+        b = prompt("На сколько его оцените?",""),
+        c = prompt("Один из последних просмотренних фильмов?",""),
+        d = prompt("на сколько оцените его", '');
+
+
+personalMovieDB.movies[a] =  b;
+personalMovieDB.movies[c] =  d;
+
+console.log(personalMovieDB);
