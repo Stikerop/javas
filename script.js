ec4eae061d800b3ac3000e247a626196bfef6b72
Artem Soldatenko <soldatenko.artem@gmail.com>
2022-09-26 16:33:33 +0300

    lesson 32

--- script.js
+++ script.js
@@ -1,31 +1,99 @@
 'use strict'
 
-const arr = [2, 13, 63, 18, 12];
-arr.sort(compareNum);
-console.log(arr);
+// let a = 5,
+//     b = a;
+//
+// b = b + 5;
+//
+// console.log(b);
+// console.log(a);
+//
+// const obj = {
+//     a: 5,
+//     b: 1
+// }
 
-function  compareNum(a, b) {
-    return a - b;
+// const copy = obj;
+//
+// copy.a = 10;
+//
+// console.log(copy );
+// console.log(obj)
+
+// function copy(mainObj) {
+//     let objCopy = {};
+//
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj [key];
+//     }
+//     return objCopy;
+// }
+//
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+//
+// const newNumbers = copy(numbers);
+//
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+//
+// console.log(newNumbers);
+// console.log(numbers);
+//
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add);
+//
+// clone.d = 12;
+// console.log(clone);
+// console.log(add);
+// const newNumberss = copy(numbers);
+//
+// newNumberss.c.x = 12;
+// console.log(newNumberss);
+//
+// const oldArray  = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+//
+// newArray[1] = 'adsafsafsaf';
+//
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//         blogs = ['wordpress', 'livejournal', 'blogger'],
+//         internet = [...video, ...blogs, 'vk', 'facebook'];
+//
+// console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
-arr[99] = 2;
-console.log(arr.length);
-console.log(arr);
 
-arr.forEach(function (item, i, arr){
-    console.log(`${i}: ${item} внутри массива ${arr}`);
-});
+const num = [2, 5, 7];
 
+log(...num);
 
-arr.pop();
-arr.push(10, 12, 14);
+const array = ['a', 'b'];
 
-console.log(arr);
+const newAarray = [...array];
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-}
+const q = {
+  one:  1,
+    two: 2
+};
 
-for (let value of arr) {
-    console.log(value)
-}
-
+const newObj = {...q};
+console.log(newObj);
