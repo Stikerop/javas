24d17709118b5474d78ddde9bb441c015774a2a0
Artem Soldatenko <soldatenko.artem@gmail.com>
2022-09-26 18:09:29 +0300

    lesson 34

--- script.js
+++ script.js
@@ -1,31 +1,79 @@
 'use strict'
 
-// let str = 'some';
-// let strObj = new String();
-//
-// console.log(typeof (str));
-// console.log(typeof (strObj));
-//
-// console.dir([1, 2, 3]);
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function (){
-        console.log("Hello");
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = prompt('На сколько оцените его', '');
+
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотренно довольно мало фильмов');
+        } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMovieDB.count > 30) {
+            console.log('Вы киноман');
+        } else {
+            console.log('Произошла ошибка');
+        }
+    },
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function () {
+      if (personalMovieDB.privat) {
+          personalMovieDB.privat = false;
+
+      }  else {
+          personalMovieDB.privat = true;
+      }
+    },
+    writeYourGenres: function () {
+        for (let i = 1; i < 2; i++) {
+            // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            //
+            // if (genre === '' || genre == null) {
+            //     console.log('Вы ввелине некоректные данные или не ввели их вовсе');
+            //     i--;
+            // } else {
+            //     personalMovieDB.genres[i - 1] = genre;
+            // }
+            let genres = prompt(`Введите ваши любимые жанры через запятую`);
+            if (genres === '' || genre == null) {
+                console.log('Вы ввелине некоректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                        personalMovieDB.genres[i] = genres.split(', ');
+                        personalMovieDB.genres.sort();
+                }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
     }
 };
 
-const jonh = Object.create(soldier);
-
-console.log(jonh);
-
-// const jonh = {
-//     health: 100
-// };
-//
-// // jonh.__proto__ = soldier;
-//
-// Object.setPrototypeOf(jonh, soldier)
-// // console.log(jonh.armor);
-jonh.sayHello()
