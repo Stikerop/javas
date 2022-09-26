df29f3fa2192e6145f48c525e342849f4d969fdc
Artem Soldatenko <soldatenko.artem@gmail.com>
2022-09-26 15:03:45 +0300

    First lesson 14

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const options = {
     name: 'test',
     width: 1024,
@@ -5,15 +7,35 @@ const options = {
     colors: {
         border: 'black',
         bg: 'red'
+    },
+    makeTest: function () {
+        console.log("Test")
     }
-}
+};
 
-// console.lo g(options.name);
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+// console.log(Object.keys(options).length);
+//
+// console.log(options["colors"] ['border'])
+// console.log(options.colors.bg);
 //
 // delete options.name;
 //
 // console.log(options);
-
-for (let key in options) {
-    console.log(`свойства ${key}, имеет значение ${options[key]}`);
-}
+// let counter =  0;
+// for (let key in options) {
+//     if (typeof (options[key]) ==='object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+//
+// console.log(counter);
