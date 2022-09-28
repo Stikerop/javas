c9c50e038740302dc5f5ab0c43cbcfaff34b18a1
Artem Soldatenko <soldatenko.artem@gmail.com>
2022-09-28 18:30:16 +0300

    Обучение по ВЛАДЛЕНУ МИНИНУ Третий урок

--- script.js
+++ script.js
@@ -1,56 +1,43 @@
-// 1 NUMBER
-// const num = 42;
-// console.log(num);
-// const float = 42.42;
-// console.log(float);
-// const pow = 10e3;
-// console.log(pow);
-// console.log(Number.MAX_SAFE_INTEGER)
-// console.log(Math.pow(2, 55 ) -1)
-// console.log(Number.MIN_SAFE_INTEGER)
-// const negative = -42;
-// console.log(negative);
-// console.log(negative < 10);
-// console.log('Max VALUE', Number.MAX_VALUE);
-// console.log('Min VALUE', Number.MIN_VALUE);
-// console.log('possitiv INFINIT', Number.POSITIVE_INFINITY);
-// console.log('Negative INFINIT', Number.NEGATIVE_INFINITY);
-// console.log('Nan', Number.NaN)
-// console.log(NaN)
-// console.log(typeof (NaN));
-// console.log(2/0);
-// console.log(Number.isFinite(false))
-// const stringInt = '40';
-// const stringFLoat = '40.42';
-// console.log(Number(stringInt)   + 2)
-// console.log(+stringInt   + 2)
-// console.log(parseFloat(stringFLoat) +2)
+// const name = 'Artem'
+// const age = 24;
+//
+// function getAge () {
+//     return age
+// }
 
-// console.log(0.4 + 0.2);
-// console.log((2 /  5) +  (1 / 5))
-// console.log(+(0.4 + 0.2).toFixed(5));
-// console.log(parseFloat((0.4 + 0.2).toFixed(1)))
+// const output = 'Привет, меня зовут ' + name + ' и мой возраст ' + age + ' лет.'
+// const output = `Привет, меня зовут ${name} и мой возраст ${getAge()} лет.`
+// console.log(output)
+// const output = `
+// <div> ЭТО СУЧКА ДИВ</div>
+// <p>а эТО П</p>
+// `
+// console.log(output)
 
-// 2 BigInt
-// console.log( 90019925474099199999999999999n);
-// console.log(-21412412521523523523523452141n);
-// console.log(parseInt(10n) - 4);
-// console.log((10n - BigInt(4)))
+// const name = 'Artem';
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.charAt(2));
+// console.log(name.indexOf(''))
+// console.log(name.endsWith('sm'))
+// console.log(name.repeat(3))
+// const string = '        password         ';
+// console.log(string.trimRight())
 
-// 3 Math
-// console.log(Math.E)
-// console.log(Math.PI)
-// console.log(Math.pow(5 , 5))
-// console.log(Math.abs(-42))
-// console.log(Math.max(42, 12321, 1312,123,312312312, 241))
-// console.log(Math.round(4.1))
-// console.log(Math.random())
-
-// 4 Example
-
-function getRandomBetween (min, max) {
-    return Math.floor( Math.random() * (max - min + 1) + min);
+function logPerson(s, name, age) {
+    if (age < 0){
+        age = 'Еще не родился'
+    }
+    return `${s[0]}${name}${s[1]}${age}${s[2]}`
 
 }
+const personName = 'Artem';
+const personName2 = 'Max';
+const personAge = 26;
+const personAge2 = -10;
 
-console.log(getRandomBetween(10, 42));
+const output = logPerson`Имя: ${personName}, Возраст: ${personAge}!`;
+const output2 = logPerson`Имя: ${personName2}, Возраст: ${personAge2}!`;
+console.log(output);
+console.log(output2);
